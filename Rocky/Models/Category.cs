f82ff777b98ee0281ad3c531afa840be2998b8d4
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-23 22:47:04 -0300

    Add Category Model

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
