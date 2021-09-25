03436a049281b3e3ecc109c655458aa35bc98028
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-24 23:41:03 -0300

    Add ApplicationTypes Controller and Views for Index and Create Actions

---
+++ Rocky/Controllers/ApplicationTypesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{   
+    public class ApplicationTypesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ApplicationTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> appTypes = _context.ApplicationTypes;
+
+            return View(appTypes);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType appType)
+        {
+            _context.ApplicationTypes.Add(appType);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
