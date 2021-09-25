922ea88fe8d24c62ba53b03610135a8051ee09a2
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-24 23:10:06 -0300

    Add ApplycationTypeModel and Table

--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -10,5 +10,7 @@ namespace Rocky.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<ApplicationType> ApplicationTypes { get; set; }
     }
 }
---
+++ Rocky/Models/ApplicationType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class ApplicationType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
