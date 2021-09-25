37f94b018578f0b401671c29828bac978ae54b34
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-25 14:54:20 -0300

    Add ApplicationTypes Edit and Delete Functionality and Views

--- Rocky/Controllers/ApplicationTypesController.cs
+++ Rocky/Controllers/ApplicationTypesController.cs
@@ -38,5 +38,64 @@ namespace Rocky.Controllers
 
             return RedirectToAction("Index");
         }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            var appType = _context.ApplicationTypes.Find(id);
+            if(appType == null)
+                return NotFound();
+
+            return View(appType);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType appType)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.ApplicationTypes.Update(appType);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(appType);
+        }
+
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            var appType = _context.ApplicationTypes.Find(id);
+            if(appType == null)
+                return NotFound();
+
+            return View(appType);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            var appType = _context.ApplicationTypes.Find(id);
+            if(appType == null)
+                return NotFound();
+
+            _context.Remove(appType);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
