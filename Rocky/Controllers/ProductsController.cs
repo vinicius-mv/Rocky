ed2ba0d44a2bae6876a50916d3d492c31665fbb2
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-25 16:23:50 -0300

    Add Product List Action and View

---
+++ Rocky/Controllers/ProductsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products;
+
+            // load related 'Category' to the 'Product' (Manual Eager Loading)
+            foreach (var product in products)
+            {
+                product.Category = _context.Categories.FirstOrDefault(c => c.Id == product.CategoryId);
+            }
+
+            return View(products);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _context.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _context.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _context.Categories.Find(id);
+            if(category == null)
+                return NotFound();
+
+            _context.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
