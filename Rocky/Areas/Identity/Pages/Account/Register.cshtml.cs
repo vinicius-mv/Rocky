b90c364b8d8dcbc1995249a2244b48b95255ee14
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-28 21:34:59 -0300

    Register First User

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,6 +92,8 @@ namespace Rocky.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, WebConstants.AdminRole); // add admin role to the current user
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
