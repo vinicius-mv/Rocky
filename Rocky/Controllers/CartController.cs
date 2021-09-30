17c42b6855d786bff6023b507169807d4120080e
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-29 21:44:22 -0300

    Delete Cart Items

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,6 +26,22 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _context.Products.Where(x => productIdInCartList.Contains(x.Id));
            
             return View(prodList);
+        }
+
+        public IActionResult Remove(int id)
+        {   
+            IList<ShoppingCart> shoppingCartList = HttpContext.Session.Get<IList<ShoppingCart>>(WebConstants.SessionCart) ?? new List<ShoppingCart>();
+
+            var productToRemove = shoppingCartList.First(x => x.ProductId == id);
+            if(productToRemove == null)
+                throw new InvalidOperationException("Failed to remove product from cart");
+
+            shoppingCartList.Remove(productToRemove);
+
+            // Update Session Store 
+            HttpContext.Session.Set<IList<ShoppingCart>>(WebConstants.SessionCart, shoppingCartList);  
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
