9bf144624d1969bfcae30550e545f3560119c9ff
Vinicius Martins <viniciusmv91@gmail.com>
2021-09-30 21:31:38 -0300

    Add Summary Get Action on Cart Controller

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -4,9 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Utility;
+using Rocky.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace Rocky.Controllers
 {
@@ -14,6 +16,9 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _context;
+
+        [BindProperty] // Automatically binds to post Actions
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext context)
         {
@@ -28,6 +33,32 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _context.Products.Where(x => productIdInCartList.Contains(x.Id));
            
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claimUserId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var claimUserId = User.FindFirstValue(ClaimTypes.Name);
+
+            IList<ShoppingCart> shoppingCartList = HttpContext.Session.Get<IList<ShoppingCart>>(WebConstants.SessionCart) ?? new List<ShoppingCart>();
+
+            List<int> productIdInCartList = shoppingCartList.Select(x => x.ProductId).ToList();
+            IEnumerable<Product> prodList = _context.Products.Where(x => productIdInCartList.Contains(x.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _context.ApplicationUsers.FirstOrDefault(x => x.Id == claimUserId.Value),
+            };
+
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)
