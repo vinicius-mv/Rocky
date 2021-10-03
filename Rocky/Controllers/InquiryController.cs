707cf995a865ed3ea83951e1bff729935a4aa097
Vinicius Martins <viniciusmv91@gmail.com>
2021-10-03 00:19:32 -0300

    Add Inquiry Controller and ViewModel

---
+++ Rocky/Controllers/InquiryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.DataAccess;
+using Rocky.DataAccess.Repository.Interfaces;
+using Rocky.Models;
+using Rocky.Utility;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    [Authorize(Roles = WebConstants.AdminRole)]
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepo;
+
+        private readonly IInquiryDetailRepository _inquiryDetailRepo;
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepo, IInquiryDetailRepository inquiryDetailRepo)
+        {
+            _inquiryHeaderRepo = inquiryHeaderRepo;
+            _inquiryDetailRepo = inquiryDetailRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rocky/ViewModels/InquiryVM.cs
@@ -0,0 +1,14 @@
+﻿using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.ViewModels
+{
+    public class InquiryVM
+    {
+        public InquiryHeader InquiryHeader { get; set; }
+        public IEnumerable<InquiryDetail> InquiryDetails { get; set; }
+    }
+}
